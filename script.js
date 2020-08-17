d25a50bdf9f43c371f945b252cc5ac0bbeaf610b
Chambers Mansour <chambersmansour97@gmail.com>
2020-08-16 22:44:47 -0500

    removed script file and added notes to html

---
+++ script.js
@@ -0,0 +1,7 @@
+//Step 1: add live date to top of calender
+//moment().format('MMM. D, YYYY [at] h:mm A z');
+
+//HAVEN'T LINKED THIS PAGE YET TO HTML
+
+//grabs id so we can place current date in the div
+var currentTime = document.getElementById("currentDay")
